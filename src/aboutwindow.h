64cde0ccb4517cfc658864f7d84d9ea13a9ebef4
Mazhar Hussain <realmazharhussain@gmail.com>
2022-11-24 23:03:53 +0500

    Add an AboutWindow

---
+++ src/aboutwindow.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "window.h"
+
+void
+show_about_window ( GtkWindow *parent_window);
--- src/app.c
+++ src/app.c
@@ -2,6 +2,7 @@
 
 #include "app.h"
 #include "window.h"
+#include "aboutwindow.h"
 
 struct _SkipperApplication
 {
@@ -10,19 +11,38 @@ struct _SkipperApplication
 
 G_DEFINE_TYPE (SkipperApplication, skipper_application, ADW_TYPE_APPLICATION);
 
-static void activate (GApplication *self) {
+
+static void
+activate (GApplication *self) {
   SkipperWindow *win = skipper_window_new (SKIPPER_APPLICATION (self));
 
   gtk_window_present (GTK_WINDOW (win));
 }
 
-static void skipper_application_class_init  (SkipperApplicationClass *klass) {
+static void
+skipper_application_class_init  (SkipperApplicationClass *klass) {
   G_APPLICATION_CLASS (klass)->activate = activate;
 }
 
-static void skipper_application_init (SkipperApplication *self) {}
+static void
+about_cb (SkipperApplication *self,
+          GAction *action)
+{
+  GtkWindow *active_window = gtk_application_get_active_window ( GTK_APPLICATION (self));
+  show_about_window (active_window);
+}
 
-SkipperApplication* skipper_application_new () {
+static void
+skipper_application_init (SkipperApplication *self) {
+  GActionMap *action_map = G_ACTION_MAP (self);
+
+  GSimpleAction *about = g_simple_action_new ("about", NULL);
+  g_signal_connect_swapped (about, "activate", G_CALLBACK(about_cb), self);
+  g_action_map_add_action (action_map, G_ACTION (about));
+}
+
+SkipperApplication*
+skipper_application_new () {
   return g_object_new(SKIPPER_TYPE_APPLICATION,
                       "application-id", "io.github.realmazharhussain.Skipper",
                       NULL);
